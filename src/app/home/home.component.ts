526e41593bd0ff73079f818b0215d013ee8ab1c1
Nicolas Oliveira <nibri.kcond2011@gmail.com>
2022-06-27 20:52:49 -0300

    exemplo de pipes e funcoes assincronas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Util from "../services/Util";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {PostalCodeService} from "../services/postal-code.service";
 
 @Component({
   selector: 'app-home',
@@ -10,7 +13,18 @@ export class HomeComponent implements OnInit {
   singUp = ""
   singIn = ""
   title = ""
-  constructor() {
+
+  userForm = new FormGroup({
+    postalCode : new FormControl("",[Validators.required]),
+    addressName : new FormControl("",[Validators.required]),
+    addressNeighborhood : new FormControl("",[Validators.required]),
+    addressNumber : new FormControl("",[Validators.required]),
+    addressSupplement : new FormControl(""),
+    addressState : new FormControl("",[Validators.required]),
+    addressCity : new FormControl("",[Validators.required]),
+  })
+
+  constructor(public postalCodeService: PostalCodeService) {
     this.singUp = "/auth/register"
     this.singIn = "/auth/login"
     this.title = "Medicinal Record"
@@ -21,4 +35,15 @@ export class HomeComponent implements OnInit {
     console.log(this.title)
   }
 
+  getAddressInfo(postalCode:string){
+    if(!Util.isNullOrEmpty(postalCode) && Util.checkEqualInputLength(postalCode, 8)){
+      this.postalCodeService.getAddressInfo(Util.cleanInt(postalCode)).subscribe((data) => {
+        this.userForm.get("addressNeighborhood")?.setValue(data.bairro);
+        this.userForm.get("addressName")?.setValue(data.logradouro);
+        this.userForm.get("addressState")?.setValue(data.uf);
+        this.userForm.get("addressCity")?.setValue(data.localidade);
+        this.userForm.get("addressSupplement")?.setValue(data.complemento);
+      });
+    }
+  }
 }
